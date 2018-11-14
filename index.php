5db3abe0af3765b1b4de595b4b00a2dbe25617ae
littlefoobar <littlefoobar@gmail.com>
2018-11-15 00:07:00 +0800

    Locally added in development;

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 echo "edited again from development branch";
-echo "Add one comment only in development"
-echo "Direct modification by A"
+echo "Add one comment only in development";
+echo "Direct modification by A";
+echo "locally added this in developmnt branch"
 ?>
