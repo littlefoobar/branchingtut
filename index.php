99793ce3155bb304d54cd8a1deba227a8ce5806e
littlefoobar <littlefoobar@gmail.com>
2018-11-15 22:05:31 +0800

    101 Commited the fix

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 echo "edited again from development branch";
 echo "Add one comment only in development";
 echo "Direct modification by A";
-echo "locally added this in developmnt branch"
-echo "Fixing the issue in this branch : add one more line"
+echo "locally added this in developmnt branch";
+echo "Fixing the issue in this branch : add one more line";
+echo "MMS-101-BRANCH-FIX"
 ?>
