b70fa00ceb0fabf292b5c35c93cff2b731a09352
littlefoobar <littlefoobar@gmail.com>
2018-11-15 21:53:20 +0800

    Commited the fix

--- index.php
+++ index.php
@@ -3,4 +3,5 @@ echo "edited again from development branch";
 echo "Add one comment only in development";
 echo "Direct modification by A";
 echo "locally added this in developmnt branch"
+echo "Fixing the issue in this branch : add one more line"
 ?>
